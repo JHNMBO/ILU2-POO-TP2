1d27658729c4c54a575f13b36a6ec2cf8b114df1
JHNMBO <johan.nzengue-mombo@univ-tlse3.fr>
2024-04-03 10:08:40 +0200

    progress

--- src/controleur/ControlAfficherMarche.java
+++ src/controleur/ControlAfficherMarche.java
@@ -9,5 +9,8 @@ public class ControlAfficherMarche {
 		this.village = village;
 	}
 
-	//TODO a completer
+	//Complete
+	public String[] donnerInfosMarche() {
+		return village.donnerEtatMarche();
+	}
 }
--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,23 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		int longueur = infosMarche.length;
+		if(longueur == 0) {
+			System.out.println("Le marché est vide, revenez plus tard");
+		}
+		else {
+			System.out.println(nomAcheteur + ", vous trouverez au marché :");
+			int i=0;
+			while(i!=longueur) {
+				String vendeur = infosMarche[i];
+				i++;
+				String quantite = infosMarche[i];
+				i++;
+				String produit = infosMarche[i];
+				i++;
+				System.out.println("- " + vendeur + " qui vend " + quantite + " " + produit);
+			}
+		}
 	}
 }
