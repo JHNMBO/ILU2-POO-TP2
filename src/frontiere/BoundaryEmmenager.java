a4f0614a1d7c2d382277f8b2ffb0dd6d4f6a8bd8
JHNMBO <johan.nzengue-mombo@univ-tlse3.fr>
2024-10-16 11:56:21 +0200

    PrendreEtal 2.4

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -11,8 +11,7 @@ public class BoundaryEmmenager {
 
 	public void emmenager(String nomVisiteur) {
 		if (controlEmmenager.isHabitant(nomVisiteur)) {
-			System.out.println(
-					"Mais vous êtes déjà un habitant du village !");
+			System.out.println("Mais vous êtes déjà un habitant du village !");
 		} else {
 			StringBuilder question = new StringBuilder();
 			question.append("Êtes-vous :\n");
@@ -27,12 +26,16 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					System.out.println("Bienvenue villageois " + nomVisiteur);
+					StringBuilder question2 = new StringBuilder();
+					question2.append("Quelle est votre force ?\n");
+					int choixUtilisateur2 = -1;
+					choixUtilisateur2 = Clavier.entrerEntier(question2.toString());
+					controlEmmenager.ajouterGaulois(nomVisiteur, choixUtilisateur2);
 					break;
 
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
@@ -40,6 +43,31 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		System.out.println("Bienvenue druide " + nomVisiteur);
+		
+		int choixUtilisateur = -1;
+		int effetPotionMax = -1;
+		int effetPotionMin = 0;
+		
+		StringBuilder question = new StringBuilder();
+		question.append("Quelle est votre force ?\n");
+		choixUtilisateur = Clavier.entrerEntier(question.toString());
+		
+		while (effetPotionMax<effetPotionMin) {
+			StringBuilder question2 = new StringBuilder();
+			question2.append("Quelle est la force de potion la plus faible que vous produisez ?\n");
+			effetPotionMin = Clavier.entrerEntier(question2.toString());
+			
+			StringBuilder question3 = new StringBuilder();
+			question3.append("Quelle est la force de potion la plus forte que vous produisez ?\n");
+			effetPotionMax = Clavier.entrerEntier(question3.toString());
+			
+			if(effetPotionMax<effetPotionMin) {
+				System.out.println("Attention Druide, vous vous �tes tromp� entre le minimum et le maximum");
+			}
+		}
+		
+		controlEmmenager.ajouterDruide(nomVisiteur, choixUtilisateur, effetPotionMin, effetPotionMax);
 	}
+	
 }
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,7 +13,22 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("Je suis désolé " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
+		} else {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un étal.");
+			if (!controlPrendreEtal.resteEtals()) {
+				System.out.println("Désolé " + nomVendeur + " je n'ai plus d'étal qui ne soit pas déjà occupé.");
+			} else {
+				installerVendeur(nomVendeur);
+				System.out.println("C'est parfait, il me reste un étal pour vous !");
+				String produit;
+				int nbProduit = 0;
+				StringBuilder question = new StringBuilder();
+				System.out.println("Il me faudrait quelques renseignements :\n");
+				produit = Clavier.entrerChaine("Quel produit souhaitez vous vendre ?");
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
